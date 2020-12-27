74732d89309d8c63929910e8c60b4ef9891ba880
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2020-12-27 01:41:28 +0100

    Improved test coverage.

--- tests/unit/Instantiate.py
+++ tests/unit/Instantiate.py
@@ -1,7 +1,7 @@
 from pathlib  import Path
 from unittest import TestCase
 
-from pyVHDLModel.VHDLModel import Design, Library, Document, Entity
+from pyVHDLModel.VHDLModel import Design, Library, Document, Entity, Architecture, PackageBody, Package, Configuration, Context
 
 
 if __name__ == "__main__":
@@ -9,9 +9,14 @@ if __name__ == "__main__":
 	print("Use: 'python -m unitest <testcase module>'")
 	exit(1)
 
+
 class Instantiate(TestCase):
 	def test_Design(self):
 		design = Design()
+
+		self.assertIsNotNone(design)
+		self.assertTrue(len(design.Documents) == 0)
+		self.assertTrue(len(design.Libraries) == 0)
 
 	def test_Library(self):
 		library = Library()
@@ -22,3 +27,18 @@ class Instantiate(TestCase):
 
 	def test_Entity(self):
 		entity = Entity("entity_1")
+
+	def test_Architecture(self):
+		architecture = Architecture("arch_1")
+
+	def test_Package(self):
+		package = Package("pack_1")
+
+	def test_PackageBody(self):
+		packageBody = PackageBody("pack_1")
+
+	def test_Context(self):
+		packageBody = Context("ctx_1")
+
+	def test_Configuration(self):
+		packageBody = Configuration("conf_1")

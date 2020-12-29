4311154a752572c6a8635e158bbbf197b518c368
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2020-12-29 01:42:35 +0100

    Prepare for next version v0.7.4.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ documentationURL =  "https://{namespace}.github.io/{projectName}".format(namespa
 # Assemble all package information
 setuptools_setup(
 	name=projectName,
-	version="0.7.3",
+	version="0.7.4",
 
 	author="Patrick Lehmann",
 	author_email="Paebbels@gmail.com",

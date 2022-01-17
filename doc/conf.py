549125006c0f364028862bba3b2aef98f516a4df
umarcor <unai.martinezcorral@ehu.eus>
2022-01-17 01:18:33 +0100

    doc: rm _templates

--- doc/conf.py
+++ doc/conf.py
@@ -41,9 +41,6 @@ release =   versionInformation.Version
 # The master toctree document.
 master_doc = 'index'
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.

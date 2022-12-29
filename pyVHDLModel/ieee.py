9c5635010cb460692269e2a287ed0185c3e04091
Patrick Lehmann <Paebbels@gmail.com>
2022-12-29 01:14:53 +0100

    Added package and library dependencies to std and ieee packages.

--- pyVHDLModel/ieee.py
+++ pyVHDLModel/ieee.py
@@ -1,6 +1,6 @@
 from pyTooling.Decorators import export
 
-from pyVHDLModel import UseClause
+from pyVHDLModel import UseClause, LibraryClause
 from pyVHDLModel.SyntaxModel import AllPackageMembersReferenceSymbol, PackageReferenceSymbol, LibraryReferenceSymbol
 from pyVHDLModel.std import PredefinedLibrary, PredefinedPackage, PredefinedPackageBody
 
@@ -24,27 +24,41 @@ class Math_Complex(PredefinedPackage):
 	def __init__(self):
 		super().__init__()
 
-		# Use clauses
-		useMathReal = (
-			AllPackageMembersReferenceSymbol(PackageReferenceSymbol("math_real", LibraryReferenceSymbol("work"))),
-		)
-		self._packageReferences.append(UseClause(useMathReal))
+		self._AddPackageClause(("work.math_real.all",))
 
 
 class Math_Complex_Body(PredefinedPackageBody):
-	pass
+	def __init__(self):
+		super().__init__()
+
+		self._AddPackageClause(("work.math_real.all",))
 
 
 class Std_logic_1164(PredefinedPackage):
-	pass
+	def __init__(self):
+		super().__init__()
+
+		self._AddPackageClause(("STD.TEXTIO.all", ))
 
 
 class Std_logic_1164_Body(PredefinedPackageBody):
 	pass
 
 
+class std_logic_textio(PredefinedPackage):
+	def __init__(self):
+		super().__init__()
+
+		self._AddPackageClause(("STD.TEXTIO.all", ))
+		self._AddLibraryClause(("IEEE", ))
+		self._AddPackageClause(("IEEE.std_logic_1164.all", ))
+
+
 class Numeric_Bit(PredefinedPackage):
-	pass
+	def __init__(self):
+		super().__init__()
+
+		self._AddPackageClause(("STD.TEXTIO.all", ))
 
 
 class Numeric_Bit_Body(PredefinedPackageBody):
@@ -56,11 +70,20 @@ class Numeric_Bit_Unsigned(PredefinedPackage):
 
 
 class Numeric_Bit_Unsigned_Body(PredefinedPackageBody):
-	pass
+	def __init__(self):
+		super().__init__()
+
+		self._AddLibraryClause(("IEEE", ))
+		self._AddPackageClause(("IEEE.numeric_bit.all", ))
 
 
 class Numeric_Std(PredefinedPackage):
-	pass
+	def __init__(self):
+		super().__init__()
+
+		self._AddPackageClause(("STD.TEXTIO.all", ))
+		self._AddLibraryClause(("IEEE", ))
+		self._AddPackageClause(("IEEE.std_logic_1164.all", ))
 
 
 class Numeric_Std_Body(PredefinedPackageBody):
@@ -68,19 +91,85 @@ class Numeric_Std_Body(PredefinedPackageBody):
 
 
 class Numeric_Std_Unsigned(PredefinedPackage):
-	pass
+	def __init__(self):
+		super().__init__()
+
+		self._AddLibraryClause(("IEEE", ))
+		self._AddPackageClause(("IEEE.std_logic_1164.all", ))
 
 
 class Numeric_Std_Unsigned_Body(PredefinedPackageBody):
+	def __init__(self):
+		super().__init__()
+
+		self._AddLibraryClause(("IEEE", ))
+		self._AddPackageClause(("IEEE.numeric_std.all", ))
+
+
+class Fixed_Float_Types(PredefinedPackage):
 	pass
+
+
+class Fixed_Generic_Pkg(PredefinedPackage):
+	def __init__(self):
+		super().__init__()
+
+		self._AddPackageClause(("STD.TEXTIO.all", ))
+		self._AddLibraryClause(("IEEE", ))
+		self._AddPackageClause(("IEEE.STD_LOGIC_1164.all", ))
+		self._AddPackageClause(("IEEE.NUMERIC_STD.all", ))
+		self._AddPackageClause(("IEEE.fixed_float_types.all", ))
+
+
+class Fixed_Generic_Pkg_Body(PredefinedPackageBody):
+	def __init__(self):
+		super().__init__()
+
+		self._AddLibraryClause(("IEEE", ))
+		self._AddPackageClause(("IEEE.MATH_REAL.all", ))
+
+
+class Fixed_Pkg(PredefinedPackage):
+	def __init__(self):
+		super().__init__()
+
+		self._AddLibraryClause(("IEEE", ))
+
+
+class Float_Generic_Pkg(PredefinedPackage):
+	def __init__(self):
+		super().__init__()
+
+		self._AddPackageClause(("STD.TEXTIO.all", ))
+		self._AddLibraryClause(("IEEE", ))
+		self._AddPackageClause(("IEEE.STD_LOGIC_1164.all", ))
+		self._AddPackageClause(("IEEE.NUMERIC_STD.all", ))
+		self._AddPackageClause(("IEEE.fixed_float_types.all", ))
+
+
+class Float_Generic_Pkg_Body(PredefinedPackageBody):
+	pass
+
+
+class Float_Pkg(PredefinedPackage):
+	def __init__(self):
+		super().__init__()
+
+		self._AddLibraryClause(("IEEE", ))
 
 
 PACKAGES = (
 	(Math_Real, Math_Real_Body),
 	(Math_Complex, Math_Complex_Body),
 	(Std_logic_1164, Std_logic_1164_Body),
+	(std_logic_textio, None),
 	(Numeric_Bit, Numeric_Bit_Body),
 	(Numeric_Bit_Unsigned, Numeric_Bit_Unsigned_Body),
 	(Numeric_Std, Numeric_Std_Body),
 	(Numeric_Std_Unsigned, Numeric_Std_Unsigned_Body),
+	(Fixed_Float_Types, None),
+	(Fixed_Generic_Pkg, Fixed_Generic_Pkg_Body),
+	(Fixed_Pkg, None),
+	(Float_Generic_Pkg, Float_Generic_Pkg_Body),
+	(Float_Pkg, None),
 )
--- pyVHDLModel/std.py
+++ pyVHDLModel/std.py
@@ -1,8 +1,10 @@
+from typing import Iterable
+
 from pyTooling.Decorators import export
 
-from pyVHDLModel import UseClause
+from pyVHDLModel import UseClause, LibraryClause
 from pyVHDLModel.SyntaxModel import Package, PackageBody, Library, PackageSymbol, AllPackageMembersReferenceSymbol, PackageReferenceSymbol, \
-	LibraryReferenceSymbol
+	LibraryReferenceSymbol, PackageMembersReferenceSymbol
 
 
 @export
@@ -12,23 +14,48 @@ class PredefinedLibrary(Library):
 
 		for packageType, packageBodyType in packages:
 			package: Package = packageType()
-			packageBody: PackageBody = packageBodyType()
-
 			package.Library = self
-			packageBody.Library = self
-
 			self._packages[package.NormalizedIdentifier] = package
-			self._packageBodies[packageBody.NormalizedIdentifier] = packageBody
+
+			if packageBodyType is not None:
+				packageBody: PackageBody = packageBodyType()
+				packageBody.Library = self
+				self._packageBodies[packageBody.NormalizedIdentifier] = packageBody
 
 
 @export
-class PredefinedPackage(Package):
+class PredefinedMixin:
+
+	def _AddLibraryClause(self, libraries: Iterable[str]):
+		symbols = [LibraryReferenceSymbol(libName) for libName in libraries]
+		libraryClause = LibraryClause(symbols)
+
+		self._contextItems.append(libraryClause)
+		self._libraryReferences.append(libraryClause)
+
+	def _AddPackageClause(self, packages: Iterable[str]):
+		symbols = []
+		for qualifiedPackageName in packages:
+			libName, packName, members = qualifiedPackageName.split(".")
+			packageSymbol = PackageReferenceSymbol(packName, LibraryReferenceSymbol(libName))
+			if members.lower() == "all":
+				symbols.append(AllPackageMembersReferenceSymbol(packageSymbol))
+			else:
+				symbols.append(PackageMembersReferenceSymbol(members, packageSymbol))
+
+		useClause = UseClause(symbols)
+		self._contextItems.append(useClause)
+		self._packageReferences.append(useClause)
+
+
+@export
+class PredefinedPackage(Package, PredefinedMixin):
 	def __init__(self):
 		super().__init__(self.__class__.__name__)
 
 
 @export
-class PredefinedPackageBody(PackageBody):
+class PredefinedPackageBody(PackageBody, PredefinedMixin):
 	def __init__(self):
 		packageSymbol = PackageSymbol(self.__class__.__name__[:-5])
 		super().__init__(packageSymbol)

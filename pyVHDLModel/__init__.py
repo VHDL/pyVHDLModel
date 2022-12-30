6bbc138a8028a1954f34801912f709b6fb9b513c
Patrick Lehmann <Paebbels@gmail.com>
2022-12-30 12:44:34 +0100

    Bump version to v0.20.2. [skip ci]

--- pyVHDLModel/__init__.py
+++ pyVHDLModel/__init__.py
@@ -39,7 +39,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2016-2022, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.20.1"
+__version__ =   "0.20.2"
 
 
 from enum            import unique, Enum, Flag, auto

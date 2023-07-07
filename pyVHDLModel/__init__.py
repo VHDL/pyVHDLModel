9f49589e16b2efcfc538403e9df4b5309e23a23a
Patrick Lehmann <Paebbels@gmail.com>
2023-07-07 23:17:22 +0200

    Reverted version to v0.27.1 for a hotfix.

--- pyVHDLModel/__init__.py
+++ pyVHDLModel/__init__.py
@@ -48,7 +48,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2016-2023, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.28.0"
+__version__ =   "0.27.1"
 
 
 from enum                      import unique, Enum, Flag, auto
